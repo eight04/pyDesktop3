8c8891b75a7437a448a24372da9064427038464a
paulb <paulb@localhost>
2005-09-25 14:20:18 +0000

    [project @ 2005-09-25 14:20:18 by paulb] Added some documentation. Changed "kfmclient openURL" to "kfmclient exec". Added a "generic" option which uses the proposed OPENER environment variable. Added Mac OS X support.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Notes on desktop application/environment support:
+
+KDE           Supports file and URL opening using kfmclient, where the openURL
+              command opens the resource and the exec command runs the
+              resource.
+
+GNOME         Supports file and URL opening using gnome-open.
+
+ROX-Filer     Supports file opening using "rox <filename>" but not URL
+              opening.
--- desktop.py
+++ desktop.py
@@ -3,6 +3,7 @@
 "Simple desktop integration for Python."
 
 import os
+import sys
 import subprocess
 
 def open(url, desktop=None):
@@ -13,24 +14,41 @@ def open(url, desktop=None):
     particular desktop environment's mechanisms to open the 'url' instead of
     guessing or detecting which environment is being used.
 
-    Suggested values for 'desktop' are "KDE" and "GNOME".
+    Suggested values for 'desktop' are "KDE", "GNOME", "Mac OS X" and "generic",
+    where "generic" uses an OPENER environment variable to open the specified
+    'url'.
 
     The process identifier of the "opener" (ie. viewer, editor, browser or
     program) associated with the 'url' is returned by this function. If the
     process identifier cannot be determined, None is returned.
     """
 
-    if desktop == "KDE" or \
+    if desktop == "generic" or \
+        desktop is None and os.environ.has_key("OPENER"):
+
+        try:
+            # NOTE: This may not handle sophisticated commands properly.
+            cmd = os.environ["OPENER"].split()
+            cmd.append(url)
+        except KeyError, exc:
+            raise OSError, "Desktop not supported (OPENER could not be used)"
+
+    elif desktop == "KDE" or \
         desktop is None and (os.environ.has_key("KDE_FULL_SESSION") or
             os.environ.has_key("KDE_MULTIHEAD")):
 
-        cmd = ["kfmclient", "openURL", url]
+        cmd = ["kfmclient", "exec", url]
 
     elif desktop == "GNOME" or \
         desktop is None and (os.environ.has_key("GNOME_DESKTOP_SESSION_ID") or
             os.environ.has_key("GNOME_KEYRING_SOCKET")):
 
         cmd = ["gnome-open", url]
+
+    elif desktop == "Mac OS X" or \
+        desktop is None and sys.platform == "darwin":
+
+        cmd = ["open", url]
 
     else:
         try:
