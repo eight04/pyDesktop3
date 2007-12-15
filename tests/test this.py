9793ce9c8e7b27a6915e5900355ac181c66bbe40
paulb <paulb@localhost>
2007-12-15 18:40:39 +0000

    [project @ 2007-12-15 18:40:39 by paulb] Tidied and added docstring.

--- tests/test this.py
+++ tests/test this.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
+
+"""
+As a script with a space in the name, useful for testing with the desktop.open
+function.
+"""
+
 import os, sys, commands
+
 os.system("kdialog " + "".join(map(commands.mkarg, sys.argv[1:])))
+
+# vim: tabstop=4 expandtab shiftwidth=4
